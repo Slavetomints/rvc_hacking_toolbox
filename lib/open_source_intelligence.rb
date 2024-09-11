fb390abdc57644849fb9cf5f84aada900cc32be3
Daisy Hardwick <161798204+Slavetomints@users.noreply.github.com>
2024-09-11 13:52:09 -0500

    fixed spelling error in module name

--- lib/open_source_intelligence.rb
+++ lib/open_source_intelligence.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # houses the open_source_inteligence functions
-module OpenSourceInteligence
+module OpenSourceIntelligence
   def self.select_mode
     puts 'open_source_inteligence'
   end
