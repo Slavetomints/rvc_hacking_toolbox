2bb8750fa4d584cefeb4c4b602608fbd71d97492
Daisy Hardwick <161798204+Slavetomints@users.noreply.github.com>
2024-09-11 10:55:22 -0500

    modularization of initialization

---
+++ lib/initialization.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require_relative 'ascii_art'
+require 'colorize'
+
+def show_title
+  puts "\n\n      Welcome to the RVC Hacking Toolbox!".colorize(:green)
+  show_ascii_title_image
+end
+
+def select_mode
+  puts '1 - Open Source Intelligence        6 - Network Traffic Analysis'
+  puts '2 - Cryptography                    7 - Scanning & Reconnaissance'
+  puts '3 - Password Cracking               8 - Web Application Exploitation'
+  puts '4 - Forensics                       9 - Wireless Access Exploitation'
+  puts '5 - Log Analysis                    10 - Enumeration & Exploitation'
+  puts "'quit' to quit"
+
+  puts("\n\nPlease select your mode: ")
+  gets.chomp
+end
--- main.rb
+++ main.rb
@@ -1,8 +1,34 @@
 # frozen_string_literal: true
 
 require 'colorize'
-require_relative 'lib/ascii_art'
+require_relative 'lib/initialization'
 
-puts "\n\n      Welcome to the RVC Hacking Toolbox!".colorize(:green)
+show_title
+mode = select_mode
 
-show_ascii_title_image
+case mode
+when '1'
+  puts '1'
+when '2'
+  puts '2'
+when '3'
+  puts '3'
+when '4'
+  puts '4'
+when '5'
+  puts '5'
+when '6'
+  puts '6'
+when '7'
+  puts '7'
+when '8'
+  puts '8'
+when '9'
+  puts '9'
+when '10'
+  puts '10'
+when 'quit'
+  exit
+else
+  puts 'Invalid mode selected'
+end
