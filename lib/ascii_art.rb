944f986ce4b22f0a3fb9428b051abb5b20dd2bc3
Daisy Hardwick <161798204+Slavetomints@users.noreply.github.com>
2024-09-11 11:02:45 -0500

    added mode files

--- lib/ascii_art.rb
+++ lib/ascii_art.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-#
 # title art by https://emojicombos.com/hacker-ascii-art
 def show_ascii_title_image # rubocop:disable Metrics/MethodLength
   puts "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡠⢤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
