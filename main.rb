48d5811edc8e1d09f403d610c48334003e0d40de
Daisy Hardwick <161798204+Slavetomints@users.noreply.github.com>
2024-10-01 15:06:20 -0500

    more error handling

--- main.rb
+++ main.rb
@@ -1,6 +1,24 @@
 # frozen_string_literal: true
 
+require 'tty-box'
+require 'tty-prompt'
 require_relative 'lib/toolbox/toolbox'
+
+@exit_flag = false
+
+def exit?
+  prompt = TTY::Prompt.new
+  options = [
+    { name: 'Exit Program', value: lambda {
+      clear_terminal
+      @exit_flag = true
+    } },
+    { name: 'Stay in program', value: -> { main } }
+  ]
+  clear_terminal
+  puts TTY::Box.error('An error occured')
+  prompt.select('Please select', options)
+end
 
 def clear_terminal
   if RUBY_PLATFORM =~ /win32|win64|\.NET|windows|cygwin|mingw32/i
@@ -10,17 +28,23 @@ def clear_terminal
   end
 end
 
-Signal.trap('INT') do
+def main
+  Signal.trap('INT') do
+    exit?
+  end
+
+  loop do
+    begin
+      Toolbox.new
+    rescue TTY::Reader::InputInterrupt
+      exit?
+    rescue StandardError
+      exit?
+    end
+    break if @exit_flag
+  end
+
   clear_terminal
-  exit
 end
 
-begin
-  Toolbox.new
-rescue TTY::Reader::InputInterrupt
-  clear_terminal
-  exit
-rescue StandardError
-  clear_terminal
-  exit
-end
+main
