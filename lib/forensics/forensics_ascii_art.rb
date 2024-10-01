b06670e1f6065ec23423bd646597e2d13ab049d9
Daisy Hardwick <161798204+Slavetomints@users.noreply.github.com>
2024-10-01 10:46:36 -0500

    Updated enumeration and exploitation and forensics titles

--- lib/forensics/forensics_ascii_art.rb
+++ lib/forensics/forensics_ascii_art.rb
@@ -2,20 +2,24 @@
 
 # This class when initialized will display the name of the subtitle from Forensics that is passed to it
 class ForensicsAsciiArt < Forensics
-  def initialize(subtitle)
-    @subtitle_hash = { 'metadata' => "/======================================================================\\
-||    _______ __          __  ___     __            __      __        ||
-||   / ____(_) /__       /  |/  /__  / /_____ _____/ /___ _/ /_____ _ ||
-||  / /_  / / / _ \\     / /|_/ / _ \\/ __/ __ `/ __  / __ `/ __/ __ `/ ||
-|| / __/ / / /  __/    / /  / /  __/ /_/ /_/ / /_/ / /_/ / /_/ /_/ /  ||
-||/_/   /_/_/\\___/    /_/  /_/\\___/\\__/\\__,_/\\__,_/\\__,_/\\__/\\__,_/   ||
-\\======================================================================/" }
-    show_subtitle(subtitle)
+  def initialize(title)
+    @subtitle_hash = { 'metadata' => '    _______ __          __  ___     __            __      __
+   / ____(_) /__       /  |/  /__  / /_____ _____/ /___ _/ /_____ _
+  / /_  / / / _ \     / /|_/ / _ \/ __/ __ `/ __  / __ `/ __/ __ `/
+ / __/ / / /  __/    / /  / /  __/ /_/ /_/ / /_/ / /_/ / /_/ /_/ /
+/_/   /_/_/\___/    /_/  /_/\___/\__/\__,_/\__,_/\__,_/\__/\__,_/  ' }
+    show_title(title)
   end
 
-  def show_subtitle(subtitle)
+  def show_title(title)
     clear_terminal
-    show_forensics
-    puts @subtitle_hash[subtitle].colorize(:red)
+    colored_title = @title_hash[title].lines.map { |line| line.colorize(:red) }.join
+    puts TTY::Box.frame(colored_title,
+                        title: { top_left: ' RVC Hacking Toolbox '.colorize(:green),
+                                 bottom_right: Toolbox.version.colorize(:green) },
+                        enable_color: true,
+                        style: { border: {
+                          fg: :red
+                        } })
   end
 end
