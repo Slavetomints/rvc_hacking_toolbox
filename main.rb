6694674d2ef097a8a44eff733ed6e9aa014ed57f
Daisy Hardwick <161798204+Slavetomints@users.noreply.github.com>
2024-10-01 15:06:11 -0500

    added error handling

--- main.rb
+++ main.rb
@@ -2,4 +2,25 @@
 
 require_relative 'lib/toolbox/toolbox'
 
-Toolbox.new
+def clear_terminal
+  if RUBY_PLATFORM =~ /win32|win64|\.NET|windows|cygwin|mingw32/i
+    system('cls')
+  else
+    system('clear')
+  end
+end
+
+Signal.trap('INT') do
+  clear_terminal
+  exit
+end
+
+begin
+  Toolbox.new
+rescue TTY::Reader::InputInterrupt
+  clear_terminal
+  exit
+rescue StandardError
+  clear_terminal
+  exit
+end
